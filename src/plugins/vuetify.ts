b73a87e24e62059378d5556142e35f3e53d73b33
Richard <richard.realtech@gmail.com>
2024-09-12 02:05:18 +0700

    first commit

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,28 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+})
